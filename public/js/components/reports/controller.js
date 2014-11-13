8a4b718acf9dd61727e3498d60cfda36071887b2
Sharon Lin <slin@hp.com>
2014-11-13 15:09:36 +0200

    WIP

--- public/js/components/reports/controller.js
+++ public/js/components/reports/controller.js
@@ -8,11 +8,20 @@
 elysiumApp.controller('ReportsController', ['$scope','$timeout','MessageBus' ,function($scope, $timeout, MessageBus) {
 
 	$scope.init = function() {
-		MessageBus.onMsg('tab.report', $scope, function (event, project) {
-			$timeout(function () {
-				$scope.project = project;
-			});
-		});
+//		MessageBus.onMsg('tab.report', $scope, function (event, project) {
+//			$timeout(function () {
+//				$scope.project = project;
+//			});
+//		});
+		//Mock data
+		$scope.project = {results:[
+		 {moduleName:'A', prediction:50, recommendation:'Add 2 developers'}
+		,{moduleName:'RRER', prediction:98, recommendation:'Add 2 developers'}
+		,{moduleName:'OAPFA', prediction:68, recommendation:'Add 2 developers'}
+		,{moduleName:'KFA kas', prediction:43, recommendation:'Add 2 developers'}
+		,{moduleName:'D', prediction:42, recommendation:'Add 2 developers'}
+		,{moduleName:'E', prediction:20, recommendation:'Add 2 developers'}
+		,{moduleName:'F', prediction:11, recommendation:'Add 2 developers'}]}
 	}
 
 	$scope.init();
---
+++ public/js/directives/count-up.js
@@ -0,0 +1,38 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: linsha
+ * Date: 11/13/14
+ * Time: 1:34 PM
+ * To change this template use File | Settings | File Templates.
+ */
+elysiumApp.directive('countUp', ['$compile',function($compile,$timeout) {
+	return {
+		restrict: 'E',
+		replace: false,
+		scope: {
+			countTo: "=countTo",
+			interval: "=interval",
+			units: '=units'
+		},
+		controller: ['$scope', '$element', '$attrs', '$timeout', function ($scope, $element, $attrs, $timeout) {
+			$scope.millis = 0;
+			if ($element.html().trim().length === 0) {
+				$element.append($compile('<span>{{millis}}%</span>')($scope));
+			} else {
+				$element.append($compile($element.contents())($scope));
+			}
+
+			var i=0;
+			function timeloop () {
+				$timeout(function () {
+					$scope.millis++;
+					//$scope.$digest();
+					i++;
+					if (i<$scope.countTo) {
+						timeloop();
+					}
+				}, $scope.interval)
+			}
+			timeloop();
+		}]
+	}}]);
